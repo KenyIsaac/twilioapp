951411f12a23f975df1c7571ad8f6cac8402ce01
Keny Isaac <keny.isaac.lm@hotmail.com>
2023-06-07 16:01:41 -0500

    cambios de lógica

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-// Función para enviar un mensaje de texto utilizando Twilio
-function enviarMensaje() {
-    const accountSid = 'ACf6f640a85529412dbbfeb0e01ea7c28a';
-    const authToken = 'e26cf268361280db8a11f05bd30c707d';
-    const client = new Twilio(accountSid, authToken);
+document.addEventListener('DOMContentLoaded', function() {
+    var navbarToggle = document.getElementById('navbarToggle');
+    var navbarMenu = document.getElementById('navbarMenu');
+    var closeMenu = document.getElementById('closeMenu');
   
-    const telefonoDestino = document.getElementById('telefono').value;
-    const mensaje = document.getElementById('mensaje').value;
-  
-    client.messages.create({
-      body: mensaje,
-      to: telefonoDestino,
-      from: '+573505305711'
-    }).then((message) => {
-      console.log('Mensaje enviado con SID: ' + message.sid);
-      alert('Mensaje enviado correctamente');
-    }).catch((error) => {
-      console.error('Error al enviar el mensaje:', error);
-      alert('Error al enviar el mensaje. Por favor, revisa la consola para más detalles.');
+    navbarToggle.addEventListener('click', function() {
+      navbarMenu.classList.toggle('navbar-menu-show');
     });
-  }
+  
+    closeMenu.addEventListener('click', function() {
+      navbarMenu.classList.remove('navbar-menu-show');
+    });
+  
+    var messageForm = document.getElementById('messageForm');
+    var responseMessage = document.getElementById('responseMessage');
+  
+    messageForm.addEventListener('submit', function(e) {
+      e.preventDefault();
+  
+      var phoneNumbers = document.getElementById('phoneNumbers').value;
+      var message = document.getElementById('message').value;
+  
+      var phoneNumbersArray = phoneNumbers.split(',');
+      var promises = [];
+  
+      phoneNumbersArray.forEach(function(number) {
+        promises.push(sendMessage(number.trim(), message));
+      });
+  
+      Promise.all(promises)
+        .then(function() {
+          responseMessage.innerText = 'Mensajes enviados exitosamente.';
+          messageForm.reset();
+        })
+        .catch(function() {
+          responseMessage.innerText = 'Hubo un error al enviar los mensajes.';
+        });
+    });
+  
+    function sendMessage(phoneNumber, message) {
+      return new Promise(function(resolve, reject) {
+        // Aquí va la lógica para enviar el mensaje
+        // ...
+        // Si el mensaje se envía exitosamente, se llama a resolve()
+        // Si hay algún error al enviar el mensaje, se llama a reject()
+      });
+    }
+  });
   
